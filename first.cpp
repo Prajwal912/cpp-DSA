78944feb573c02e94e7245741f56dda42c16ec98
prajwal <prajwalshah912@gmail.com>
2023-08-15 23:52:13 +0530

    data types in cpp

--- first.cpp
+++ first.cpp
@@ -3,4 +3,22 @@ using namespace std;
 
 int main() {
     cout<< "Namaste Dunia" << endl;
+
+    int a = 124;
+    cout << a << endl;
+
+    char b = 'p';
+    cout << b << endl;
+
+    bool bl = false;
+    cout<< bl << endl;
+
+   float point = 1.23673234;
+   cout<< point << endl;
+
+   double point2 = 1.24456;
+   cout<<point2<<endl;
+
+   int size = sizeof(point2);
+   cout<<"the size  is :" << size << endl;
 }
