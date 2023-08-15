d27567c6e3d149f75b1a0e77b14d25861f4e286f
prajwal <prajwalshah912@gmail.com>
2023-08-16 00:04:56 +0530

    converted data type implicitly

--- first.cpp
+++ first.cpp
@@ -1,24 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
-    cout<< "Namaste Dunia" << endl;
+int main()
+{
+    //     cout<< "Namaste Dunia" << endl;
 
-    int a = 124;
-    cout << a << endl;
+    //     int a = 124;
+    //     cout << a << endl;
 
-    char b = 'p';
-    cout << b << endl;
+    //     char b = 'p';
+    //     cout << b << endl;
 
-    bool bl = false;
-    cout<< bl << endl;
+    //     bool bl = false;
+    //     cout<< bl << endl;
 
-   float point = 1.23673234;
-   cout<< point << endl;
+    //    float point = 1.23673234;
+    //    cout<< point << endl;
 
-   double point2 = 1.24456;
-   cout<<point2<<endl;
+    //    double point2 = 1.24456;
+    //    cout<<point2<<endl;
 
-   int size = sizeof(point2);
-   cout<<"the size  is :" << size << endl;
+    //    int size = sizeof(point2);
+    //    cout<<"the size  is :" << size << endl;
+
+    // implicit type casting
+
+    int num = 'p';
+    cout << num << endl;
+
+    char chara = 126;
+    cout << chara << endl; //126 ki val ~ hai
+
+    bool newbl = 89;
+    cout << newbl << endl; //since it is an non zero val so it will be 1
+
+    char newCh = false;
+    cout << newCh << endl; //since it is false that means 0 so it will come null
 }
