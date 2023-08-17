570912186ad9b41da0ed8bdb19ae735eb0b0b0c7
prajwal <prajwalshah912@gmail.com>
2023-08-17 05:43:47 +0530

    cin to enter any value

---
+++ second.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // int n;
+    // cin>>n;
+    // cout<< "prajwal" << endl;
+
+    // if(n>0){
+    //     cout << n << " " << "is +ve";
+    // }else{
+    //     cout << n << " "  << "is -ve";
+    // }
+
+
+
+}
