43a4a5abda6cf75211979a7066cdb2fe562705c1
prajwal <prajwalshah912@gmail.com>
2023-08-18 00:44:56 +0530

    some basic problems using while loops

---
+++ while_loop.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout<< "enter your desired end";
+    // int end;
+    // cin>> end;//10
+    // int start = 1;
+
+    // while(start<=end){
+    //  cout<<start<<endl;
+    //  start++;
+    // }
+
+    //   int start = 1;
+    //   int end;
+    //   cout<<"enter a val";
+    //   cin>>end;
+    //   int sum = 0;
+    //   while(start<=end){
+    //     sum = sum + start;
+    //     start++;
+    //   }
+    //     cout<<sum<<endl;
+
+    /////sum of even numbers 1 to 10
+    // int start = 2;
+
+    // cout << "enter a ending number : ";
+    // int end;
+    // cin >> end;
+
+    // int sum = 0;
+
+    // while (start <= end)
+    // {
+    //     sum = sum + start;
+    //     start += 2;
+    // }
+    // cout << sum << endl;
+
+    // return 0;
+
+    ////farenheit to celcius
+    // cout<<"enter f: ";
+    // int f;
+    // cin>>f;
+
+    // double c = (f - 32) * 5.0/9.0;
+
+    // cout<<"the value of converted celcius from f is : "<< c;
+
+    ////given number is prime or not
+
+    // cout<<"enter a value :" ;
+    // int input;
+    // cin >> input; // 5
+
+    // int start = 2;
+
+    // while (start < input)
+    // {
+    //     if (input % start == 0)
+    //     {
+    //         cout << "not a prime" << start <<  endl;
+    //     }
+    //     else
+    //     {
+    //         cout << "prime" << start << endl;
+    //     }
+    //         start++;
+    // }
+
+    
+}
