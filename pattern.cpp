aa354b4f68b39ed1f255e50b57e15b169cf52146
prajwal <prajwalshah912@gmail.com>
2023-08-19 18:57:17 +0530

    patterns

---
+++ pattern.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // cout<<"enter target";
+    // int input;
+    // cin>>input;
+
+    // int i = 1;
+
+    // //first loop is for row
+    // while(i<=input){
+
+    //     int j = 1;
+    //     while(j<=input){
+    //        cout<<"p";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // int i = 1;
+
+    // while(i<=input){
+
+    //     int j = 1;
+    //     while(j<=input){
+    //     cout<<j;
+    //     j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // int reachTo;
+    // cin >> reachTo;
+    // int i = 1;
+
+    // while (i <= reachTo)
+    // {
+
+    //     int j = 1;
+
+    //     while (j <= reachTo)
+    //     {
+    //         cout << reachTo - i + 1;
+    //         j++;
+    //     }
+
+    //     cout << endl;
+    //     i++;
+    // }
+
+
+//start from 1 and ends with n count
+    // int i = 1;
+    // int n;
+    // cin >> n;
+    // int end = 1;
+    // while (i <= n)
+    // {
+
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << end;
+    //         end++;
+    //         j++;
+    //     }
+
+    //     cout << endl;
+    //     i++;
+    // }
+
+    
+}
